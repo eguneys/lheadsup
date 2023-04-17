77a38b709d2610dfd4c240172ccf1959087bdd6b
Emre Guneyler <eguneys@gmail.com>
2023-04-17 09:09:13 +0300

    hand eval

--- src/hand.ts
+++ src/hand.ts
@@ -2,14 +2,72 @@ export class Hand {
 
 }
 
+
+const Rank2 = 20
+const Rank3 = Rank2 * 20
+const Rank4 = Rank3 * 20
+const Rank5 = Rank4 * 20
+const Rank6 = Rank5 * 20
+
+export const HandEvalFunctions = {
+  high: (a, b, c, d, e) => Rank6 * 0 + Rank5 * a + Rank4 * b + Rank3 * c + Rank2 * d + e,
+  pair: (a, b, c, d) => Rank6 * 1 + Rank4 * a + Rank3 * b + Rank2 * c + d,
+  pair2: (a, b, c) => Rank6 * 2 + Rank3 * a + Rank2 * b + c,
+  set: (a, b, c) => Rank6 * 3 + Rank3 * a + Rank2 * b + c,
+  full: (a, b) => Rank6 * 4 + Rank2 * a + b,
+  straight: (a) => Rank6 * 5 + a,
+  flush: (a) => Rank6 * 6 + a,
+  quad: (a) => Rank6 * 7 + a,
+  sflush: (a) => Rank6 * 8 + a,
+}
+
 export type Card = string
 export class HandRank {
 
+  static from_fen = (fen: string) => {
+    let [high, ...rest] = fen.split(' ')
+
+    switch (high) {
+      case 'high':
+        return HandRank.high(rest)
+      case 'pair':
+        return HandRank.pair(rest)
+      case 'pair2':
+        return HandRank.pair2(rest)
+      case 'set':
+        return HandRank.set(rest)
+      case 'full':
+        return HandRank.full(rest)
+      case 'straight':
+        return HandRank.straight(rest[0])
+      case 'flush':
+        return HandRank.flush(rest[0])
+      case 'quad':
+        return HandRank.quad(rest[0])
+      case 'sflush':
+        return HandRank.sflush(rest[0])
+    }
+  }
+
   static quad = quad => new HandRank(quad)
   static high = high => new HandRank(undefined, high)
+  static pair = pair => new HandRank(undefined, undefined, pair)
+  static pair2 = pair2 => new HandRank(undefined, undefined, undefined, pair2)
+  static set = set => new HandRank(undefined, undefined, undefined, undefined, set)
+  static full = full => new HandRank(undefined, undefined, undefined, undefined, undefined, full)
+  static straight = straight => new HandRank(undefined, undefined, undefined, undefined, undefined, undefined, straight)
+  static flush = flush => new HandRank(undefined, undefined, undefined, undefined, undefined, undefined, undefined, flush)
+  static sflush = sflush => new HandRank(undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, sflush)
 
   constructor(public quad?: Card, 
-              public high?: [Card, Card, Card, Card, Card]) {}
+              public high?: [Card, Card, Card, Card, Card],
+             public pair?: [Card, Card, Card, Card],
+             public pair2?: [Card, Card, Card],
+             public set?: [Card, Card, Card],
+             public full?: [Card, Card],
+             public straight?: Card,
+             public flush?: Card,
+             public sflush?: Card) {}
 
   get fen() {
     if (this.quad) {
@@ -18,6 +76,30 @@ export class HandRank {
     if (this.high) {
       return `high ${this.high.join(' ')}`
     }
+    if (this.full) {
+      return `full ${this.full.join(' ')}`
+    }
+    if (this.set) {
+      return `set ${this.set.join(' ')}`
+    }
+    if (this.pair2) {
+      return `pair2 ${this.pair2.join(' ')}`
+    }
+    if (this.pair) {
+      return `pair ${this.pair.join(' ')}`
+    }
+
+    if (this.sflush) {
+      return `sflush ${this.sflush}`
+    }
+
+    if (this.straight) {
+      return `straight ${this.straight}`
+    }
+
+    if (this.flush) {
+      return `flush ${this.flush}`
+    }
   }
 
   get eval() {
@@ -25,9 +107,43 @@ export class HandRank {
   }
 }
 
+const cards = 'AKQJT98765432'.split('')
+export const card_rank = (card: string) => 14 - cards.indexOf(card)
+
 export function rank_eval(rank: HandRank) {
-  return 0
+  if (rank.high) {
+    let [a, b, c, d, e] = rank.high.map(card_rank)
+    return HandEvalFunctions.high(a, b, c, d, e)
+  } else if (rank.pair) {
+    let [a, b, c, d] = rank.pair.map(card_rank)
+    return HandEvalFunctions.pair(a, b, c, d)
+  } else if (rank.pair2) {
+    let [a, b, c] = rank.pair2.map(card_rank)
+    return HandEvalFunctions.pair2(a, b, c)
+  } else if (rank.set) {
+    let [a, b, c] = rank.set.map(card_rank)
+    return HandEvalFunctions.set(a, b, c)
+  } else if (rank.full) {
+    let [a, b] = rank.full.map(card_rank)
+    return HandEvalFunctions.full(a, b)
+  } else if (rank.straight) {
+    let a = card_rank(rank.straight)
+    return HandEvalFunctions.straight(a)
+  } else if (rank.flush) {
+    let a = card_rank(rank.flush)
+    return HandEvalFunctions.flush(a)
+  } else if (rank.quad) {
+    let a = card_rank(rank.quad)
+    return HandEvalFunctions.quad(a)
+  } else if (rank.sflush) {
+    let a = card_rank(rank.sflush)
+    return HandEvalFunctions.sflush(a)
+  }
+  else return 0
 }
+
+const sort_higher = (a, b) => card_rank(b) - card_rank(a)
+const sort_higher_card = (a, b) => card_rank(b[0]) - card_rank(a[0])
 
 export function rank5(hand: Array<Card>): HandRank {
 
@@ -39,12 +155,61 @@ export function rank5(hand: Array<Card>): HandRank {
 
   if (histogram.length === 2) {
     let [high, count] = histogram[0]
+    let [high2, count2] = histogram[1]
     if (count === 4) {
       return HandRank.quad(high)
     }
+    if (count === 3 && count2 === 2) {
+
+      return HandRank.full([high, high2])
+    }
+  }
+  if (histogram.length === 3) {
+    let [high, count] = histogram[0]
+    let [high2, count2] = histogram[1]
+    let [high3, count3] = histogram[2]
+
+    if (count === 3 && count2 === 1 && count3 === 1) {
+      return HandRank.set([high, high2, high3])
+    }
+
+    if (count === 2 && count2 === 2 && count3 === 1) {
+      return HandRank.pair2([...[high, high2].sort(sort_higher), high3])
+    }
   }
 
-  return HandRank.high([5, 4, 3, 2, 1])
+  if (histogram.length === 4) {
+    let [high, count] = histogram[0]
+    let [high2, count2] = histogram[1]
+    let [high3, count3] = histogram[2]
+    let [high4, count4] = histogram[3]
+
+
+    return HandRank.pair([high, high2, high3, high4].sort(sort_higher))
+  }
+
+  let is_flush = hand.every(_ => _[1] === hand[0][1])
+
+  let is_straight = false
+  hand.sort(sort_higher_card)
+  let top = hand[0], bottom = hand[4]
+  if (card_rank(top[0]) - card_rank(bottom[0]) === 4) {
+    is_straight = true
+  }
+  if (is_straight && is_flush) {
+    return HandRank.sflush(top[0])
+  }
+
+  if (is_straight) {
+    return HandRank.straight(top[0])
+  }
+
+  if (is_flush) {
+    return HandRank.flush(top[0])
+  }
+
+
+  return HandRank.high(hand.map(_ => _[0]))
 }
 
 export function hand_rank(hand: Array<Card>): HandRank {
---
+++ tests/hand_eval.test.ts
@@ -0,0 +1,67 @@
+import { it, expect } from 'vitest'
+import { Hand, HandRank, hand_rank, rank_eval } from '../src'
+import { HandEvalFunctions } from '../src'
+
+const { high, quad, sflush, flush, straight, pair, pair2, set, full } = HandEvalFunctions
+
+let hands = [
+  `2c Ts 7h Kd 5d 9c 3s 6h Qh`,
+  `Jc 6c 5s 9h 8d Qs 2h 7s Tc`,
+  `8h 4s 6c Qh 2d 7d 5c Th Js`,
+  `9d 7c 5s 4h 6d 3c Kc Qd Jh`,
+  `Qs 3h 9c 5d 8c 4s 7d Jc Th`,
+  `Kd 2h 7c 9s 4c 8s Qd Tc 5h`,
+  `6c 8h Ts 7d 9h 4d 2s Jh Qc`,
+  `5s 9d 8d 6h Ks Qc Jd 2c 7s`,
+  `Jc 3d Ts 2h 8c 6s Kd 9h 7d`,
+  `Qh 4d 7c 3s 8h 5c 9d Kc Js`
+]
+
+const fen_hand_rank = (fen: string) => hand_rank(fen.split(' ')).fen
+const fen_rank_eval = (fen: string) => rank_eval(HandRank.from_fen(fen))
+
+it('evaluate hand to rank', () => {
+
+  expect(fen_hand_rank(`Qd Qh Qc Qs 5d 2c 8s`)).toBe(`quad Q`)
+  expect(fen_hand_rank(`Kc Kh Ks Jd Jc 6h 7s`)).toBe(`full K J`)
+  expect(fen_hand_rank(`7c 7s 7h Kd Jh 8s 3d`)).toBe(`set 7 K J`)
+  expect(fen_hand_rank(`Qh Qd 8s 8d 6c 4s 2h`)).toBe(`pair2 Q 8 6`)
+  expect(fen_hand_rank(`8h 9h Th Jh Qh 7d 2c`)).toBe(`sflush Q`)
+
+
+  expect(fen_hand_rank(`Kc Kh 2s 9h 3d 6c 7d`)).toBe(`pair K 9 7 6`)
+  expect(fen_hand_rank(`5c 6d 7s 8h 9h Qc 2d`)).toBe('straight 9')
+  expect(fen_hand_rank(`Ah 2h 5h 8h Kh 7s Jc`)).toBe('flush A')
+})
+
+it('evaluate rank to number', () => {
+  const T = 10, J = 11, Q = 12, K = 13, A = 14
+
+  expect(fen_rank_eval('high 7 6 4 3 2')).toBe(high(7, 6, 4, 3, 2))
+  expect(fen_rank_eval('high K J 9 3 2')).toBe(high(K, J, 9, 3, 2))
+  expect(fen_rank_eval('high A K J 3 2')).toBe(high(A, K, J, 3, 2))
+  expect(fen_rank_eval('pair K 9 7 6')).toBe(pair(K, 9, 7, 6))
+  expect(fen_rank_eval('pair A J 3 2')).toBe(pair(A, J, 3, 2))
+  expect(fen_rank_eval('pair 2 A K J')).toBe(pair(2, A, K, J))
+  expect(fen_rank_eval(`pair2 Q 8 6`)).toBe(pair2(Q, 8, 6))
+  expect(fen_rank_eval(`pair2 A 3 2`)).toBe(pair2(A, 3, 2))
+  expect(fen_rank_eval(`pair2 3 A 9`)).toBe(pair2(3, A, 9))
+  expect(fen_rank_eval(`set 2 K J`)).toBe(set(2, K, J))
+  expect(fen_rank_eval(`set T K J`)).toBe(set(T, K, J))
+  expect(fen_rank_eval(`set A K J`)).toBe(set(A, K, J))
+  expect(fen_rank_eval(`full J T`)).toBe(full(J, T))
+  expect(fen_rank_eval(`full A 5`)).toBe(full(A, 5))
+  expect(fen_rank_eval(`full K 2`)).toBe(full(K, 2))
+  expect(fen_rank_eval('straight 9')).toBe(straight(9))
+  expect(fen_rank_eval('straight A')).toBe(straight(A))
+  expect(fen_rank_eval('straight 5')).toBe(straight(5))
+  expect(fen_rank_eval('flush T')).toBe(flush(T))
+  expect(fen_rank_eval('flush A')).toBe(flush(A))
+  expect(fen_rank_eval('quad T')).toBe(quad(T))
+  expect(fen_rank_eval('quad A')).toBe(quad(A))
+  expect(fen_rank_eval('sflush 9')).toBe(sflush(9))
+  expect(fen_rank_eval('sflush A')).toBe(sflush(A))
+  expect(fen_rank_eval('sflush 5')).toBe(sflush(5))
+})
+
+
