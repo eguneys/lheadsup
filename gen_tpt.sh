df0c6cc0bb4aeacd4d1515ef0106482729d9baba
Emre Guneyler <eguneys@gmail.com>
2023-06-18 21:06:23 +0300

    mkdir data

--- gen_tpt.sh
+++ gen_tpt.sh
@@ -7,10 +7,19 @@ echo "Generating HandRanks.dat file."
 if [ -f $FILE ]; then
   echo "$FILE exists."
 else 
+
   if ! [ -f "dist/gen_tpt" ]; then
+    echo "Compiling gen_tpt program."
     gcc gen_tpt/generate_table.cpp gen_tpt/fast_eval.cpp -o dist/gen_tpt
   fi
 
+  folder=$(dirname "$FILE")
+
+  if [ ! -d "$folder" ]; then
+    mkdir -p "$folder"
+  fi
+
+  echo "Running gen_tpt"
   "dist/gen_tpt" "$FILE"
 
   if [ $? -eq 0 ]; then
